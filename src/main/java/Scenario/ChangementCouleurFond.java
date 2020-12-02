31deccf817704e6525fb3884370707fad25e1972
Tristanfrd <Tristanfrd@users.noreply.github.com>
2020-12-02 16:02:01 +0100

    Classe CCF

--- src/main/java/Scenario/ChangementCouleurFond.java
+++ src/main/java/Scenario/ChangementCouleurFond.java
@@ -4,11 +4,41 @@
  * and open the template in the editor.
  */
 package Scenario;
-
+import bandeau.Bandeau;
+import java.awt.Color;
 /**
  *
  * @author frede
  */
-public class ChangementCouleurFond {
+public class ChangementCouleurFond extends Effet{
+
+    private Color couleur;
+
+    public ChangementCouleurFond(Color couleur, String nom, Bandeau b) {
+        super(nom, b);
+        this.couleur = couleur;
+    }
+
+    public Color getCouleur() {
+        return couleur;
+    }
+
+    public void setCouleur(Color couleur) {
+        this.couleur = couleur;
+    }
+
+    @Override
+    public String toString() {
+        return "ChangementCouleurFond{" + "couleur=" + couleur + '}';
+    }
+
+    
+    
+    @Override
+    public void effctuerEffet() {
+        getBandeau().setMessage(getNom());
+        getBandeau().setBackground(couleur);
+        getBandeau().sleep(1000);
+    }
     
 }
