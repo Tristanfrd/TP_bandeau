a9c786a9dc1f01eb25c04132dc1652e85ddfde5a
Tristanfrd <Tristanfrd@users.noreply.github.com>
2020-12-02 15:42:57 +0100

    Premier Commit, début scénar, classe effet, CgtCoulTxt

---
+++ src/main/java/Scenario/ChangementCouleurTexte.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Scenario;
+import bandeau.Bandeau;
+import java.awt.Color;
+/**
+ *
+ * @author frede
+ */
+public class ChangementCouleurTexte extends Effet {
+    
+    private Color couleur;
+
+    public ChangementCouleurTexte(Color couleur, String nom, Bandeau b) {
+        super(nom, b);
+        this.couleur = couleur;
+    }
+
+    public Color getCouleur() {
+        return couleur;
+    }
+
+    public void setCouleur(Color couleur) {
+        this.couleur = couleur;
+    }
+
+    @Override
+    public String toString() {
+        return "ChangementCouleurTexte{" + "couleur=" + couleur + '}';
+    }
+
+    @Override
+    public void effctuerEffet() {
+        
+    }
+    
+    
+}
